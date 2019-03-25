09d9185320b57d897c054dee5340622c29488bbc
Addison-Stavlo <AddieStavlo@gmail.com>
2019-03-25 19:08:51 -0400

    fixed first solution finding eqs

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,13 +8,33 @@ import math
 
 
 def eating_cookies(n, cache=None):
+    num_permutations = 0
+    solutions = []
+    # find solution with most 3's
+    if n % 3 == 0:
+        sol = [3]*int(n/3)
+        solutions.append(sol)
+    elif n % 3 == 1:
+        sol = [3]*(math.floor(n/3))
+        sol.append(1)
+        solutions.append(sol)
+    else:
+        sol1 = [3]*math.floor(n/3)
+        sol1.append(2)
+        solutions.append(sol1)
+        sol2 = [3]*math.floor(n/3)
+        sol2.append(1)
+        sol2.append(1)
+        solutions.append(sol2)
+    return solutions
 
-    pass
+
+print(eating_cookies(2))
 
 
 def number_of_permutations(list):
-    # for list of size N with repeating elements of amounts A, B, etc..
-    # number of permutations is given by N! / (A!B!...);
+        # for list of size N with repeating elements of amounts A, B, etc..
+        # number of permutations is given by N! / (A!B!...);
     N = len(list)
     A = list.count(3)
     B = list.count(2)
