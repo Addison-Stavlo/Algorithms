f914f6256b0f270a4e4ceaaa9fa04748863e4805
Addison-Stavlo <AddieStavlo@gmail.com>
2019-03-26 14:50:05 -0400

    added recursive cache solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,9 +7,9 @@ import math
 # recursive solution
 
 
+# my preferred solution
 def eating_cookies(n, cache=None):
     # realize that sequence is s[n] = s[n-1] + s[n-2] + s[n-3] for All n > 2
-
     # handle base cases
     if n < 2:
         return 1
@@ -24,14 +24,31 @@ def eating_cookies(n, cache=None):
     # fill in remaining sequence up to s[n]
     for i in range(3, n + 1):
         sequence[i] = sequence[i - 3] + sequence[i - 2] + sequence[i - 1]
-
+    # we dont need no stinking caches
     return sequence[n]
 
+
+def eating_cookies_cache(n, cache=None):
+    # cache recursion solution
+    if cache == None:
+        cache = [0 for i in range(n+1)]
+    if n < 2:
+        return 1
+    elif n == 2:
+        return 2
+    elif cache[n] > 0:
+        return cache[n]
+    else:
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
 
 # below 2 functions = solution before realizing the sequence...
 # it is faster than recursive solution,
 # passes small tests and test for 50, fails test 100 and 500
 # not quite sure why... off by 1 quadrillionth of a percent...
+
+
 def number_of_permutations(solution):
         # for list of size N with repeating elements of amounts A, B, etc..
         # number of permutations is given by N! / (A!B!...);
